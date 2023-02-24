52c8684a974c4d12e4bc673e9387342f901eabf8
ZoHuan <31094955+ZoHuan@users.noreply.github.com>
2023-02-24 11:09:52 +0800

    Delete src directory

--- src/App.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <Map />
-</template>
-
-<script setup lang="ts">
-import Map from '@/views/map/3DMap/3DMap.vue'
-</script>
-
-<style scoped></style>
--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import "@/core/rem";
-import "normalize.css/normalize.css";
-import "@/assets/font/font.css";
-import "@/assets/styles/global.less";
-
-createApp(App).mount("#app");
